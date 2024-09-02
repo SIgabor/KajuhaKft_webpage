40ef282ea8eb06113c7ae99eff2878ed03402bf6
SIgabor <79259040+SIgabor@users.noreply.github.com>
2024-09-02 22:12:12 +0200

    Documents subpage update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function sendMail() {
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
+        phoneNumber: document.getElementById("phone-number").value,
         subject: document.getElementById("subject").value,
         message: document.getElementById("message").value,
 
@@ -27,6 +28,7 @@ function sendMail() {
         emailjs.send(serviceID, templateID, params).then((res) => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
+            document.getElementById("phone-number").value = "";
             document.getElementById("subject").value = "";
             document.getElementById("message").value = "";
             document.getElementById("error-message").style.display = "none";
